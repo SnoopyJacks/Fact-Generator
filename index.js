d861439a047bd36c7c57c5ef5bae0ed2e06a9762
Justine Ziola <t.nichole.2807@gmail.com>
2025-02-19 05:05:22 -0700

    it is debugged
    Make sure to watch the capitalization on words in JS 👍

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
     button.addEventListener("click", async () => {
         try {
-            //Get random fact ftom API
+            //Get random fact from API
             const response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
             const data = await response.json();
 
             //Fact displayed using typewriter
             factBox.innerHTML = ""; //clear previous text
-            const typewriter = new typewriter(factBox, {
+            const typewriter = new Typewriter(factBox, {
                 loop: false,
                 delay: 40,
             });
 
             typewriter
-                .typestring(data.text)  //API returns fact as words
+                .typeString(data.text)  //API returns fact as words
                 .start();
         }   catch (error) {
             factBox.innerHTML = "Oops! Something went wrong.  Please try again.";
             console.error("Error fetching fact:", error);
-     }
-}); 
+         }
+    });
+});
