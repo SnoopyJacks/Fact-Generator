2879a36737523a0011db7c409f59128b63d33eda
Justine Ziola <t.nichole.2807@gmail.com>
2025-02-19 05:21:12 -0700

    switched API
    Other one did not have capabilities to input a specific topic

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("button");
     const factBox = document.getElementById("fact-box");
+    const inputField = document.querySelector("input")
 
     button.addEventListener("click", async () => {
+        const topic = inputField.value.trim();
+        if (!topic) {
+            factBox.innerHTML="Please enter a topic";
+            return;
+        }
+
+
         try {
-            //Get random fact from API
-            const response = await fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+            //Get random fact from wikipedia API
+            const response = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(topic)}`);
             const data = await response.json();
 
             //Fact displayed using typewriter
+        if (data.extract) {
             factBox.innerHTML = ""; //clear previous text
             const typewriter = new Typewriter(factBox, {
                 loop: false,
@@ -16,8 +25,11 @@ document.addEventListener("DOMContentLoaded", () => {
             });
 
             typewriter
-                .typeString(data.text)  //API returns fact as words
+                .typeString(data.extract)  
                 .start();
+        } else {
+            factBox.innerHTML = "No fact found on this topic. Please try again."
+            }
         }   catch (error) {
             factBox.innerHTML = "Oops! Something went wrong.  Please try again.";
             console.error("Error fetching fact:", error);
